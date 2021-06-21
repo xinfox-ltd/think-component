83c9d337f13e09d1b1b24c6ab10820c13d7f553f
韦明磊 <nicolaslei@163.com>
2021-06-21 11:22:20 +0800

    fix: 修复Authorization无法获取的BUG

--- src/Provider/Request.php
+++ src/Provider/Request.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 
 namespace XinFox\ThinkPHP\Provider;
 
+use think\App;
 use think\facade\Log;
 use XinFox\Auth\Auth;
 use XinFox\Auth\Guest;
@@ -22,20 +23,27 @@ class Request extends \think\Request
 
         // 默认访客
         $visitor = new Guest();
+        $this->setVisitor($visitor);
+    }
 
-        $authorization = $this->header('Authorization');
+    public static function __make(App $app): Request
+    {
+        $request = parent::__make($app);
+
+        $authorization = $request->header('Authorization');
         if ($authorization !== null) {
             $token = trim(preg_replace('/^(?:\s+)?Bearer\s/', '', $authorization));
             if ($token) {
                 try {
                     $visitor = app(Auth::class)->user($token);
+                    $request->setVisitor($visitor);
                 } catch (\Exception $e) {
                     Log::info($e);
                 }
             }
         }
 
-        $this->setVisitor($visitor);
+        return $request;
     }
 
     public function getVisitor(): VisitorInterface
